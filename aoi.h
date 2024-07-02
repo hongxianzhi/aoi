f882cc9ac01ec2c95a36773c3060da7154f1c714
hongxianzhi <hongxianzhi@live.com>
2024-07-02 14:09:40 +0800

    增加api导出

--- aoi.h
+++ aoi.h
@@ -4,25 +4,40 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+//添加导出宏AOI_BUILD_AS_DLL， AOI_API
+#if defined(_WIN32)
+	#if defined(AOI_BUILD_AS_DLL)	/* { */
+		#define AOI_API __declspec(dllexport)
+	#else						/* }{ */
+		#define AOI_API __declspec(dllimport)
+	#endif						/* } */
+#else
+	#define AOI_API extern
+#endif
+
 struct aoi_space;
 
 typedef void * (*aoi_Alloc)(void *ud, void * ptr, size_t sz);
 typedef void (message_handler)(struct aoi_space* space, void* userdata);
 
-struct aoi_space * aoi_create(aoi_Alloc alloc, void *ud);
-struct aoi_space * aoi_new();
-void aoi_release(struct aoi_space *);
-int aoi_gen_id(struct aoi_space *space);
+AOI_API struct aoi_space * aoi_create(aoi_Alloc alloc, void *ud);
+AOI_API struct aoi_space * aoi_new();
+AOI_API void aoi_release(struct aoi_space *);
+AOI_API int aoi_gen_id(struct aoi_space *space);
 
 // w(atcher) m(arker) d(rop)
-void aoi_update(struct aoi_space * space , uint32_t id, const char * mode , float pos[3], float radius);
-void aoi_message(struct aoi_space *space);
+AOI_API void aoi_update(struct aoi_space * space , uint32_t id, const char * mode , float pos[3], float radius);
+AOI_API void aoi_message(struct aoi_space *space);
 
 //move
-void aoi_move_to(struct aoi_space *space, uint32_t id, float speed, float pos[3]);
-void aoi_set_speed(struct aoi_space *space, uint32_t id, float speed);
-void aoi_apply_move(struct aoi_space *space, float delta);
-void aoi_cancel_move(struct aoi_space *space, uint32_t id);
+AOI_API void aoi_move_to(struct aoi_space *space, uint32_t id, float speed, float pos[3]);
+AOI_API void aoi_set_speed(struct aoi_space *space, uint32_t id, float speed);
+AOI_API void aoi_apply_move(struct aoi_space *space, float delta);
+AOI_API void aoi_cancel_move(struct aoi_space *space, uint32_t id);
 
 //user callback
 //预定义事件类型、数据结构
@@ -51,17 +66,21 @@ typedef struct _free_aoi_space_data {
 	struct aoi_space* space;
 } free_aoi_space_data;
 
-void* aoi_get_user_data(struct aoi_space *space, char* data_id);
-void* aoi_create_user_data(struct aoi_space *space, char* data_id, size_t sz);
-void aoi_push_message_handler(struct aoi_space *space, char* message_id, message_handler* cb);
-void aoi_pop_message_handler(struct aoi_space *space, char* message_id, message_handler* cb);
-void aoi_fire_message(struct aoi_space *space, char* message_id, void* userdata);
-char* aoi_current_message_id(struct aoi_space *space);
+AOI_API void* aoi_get_user_data(struct aoi_space *space, char* data_id);
+AOI_API void* aoi_create_user_data(struct aoi_space *space, char* data_id, size_t sz);
+AOI_API void aoi_push_message_handler(struct aoi_space *space, char* message_id, message_handler* cb);
+AOI_API void aoi_pop_message_handler(struct aoi_space *space, char* message_id, message_handler* cb);
+AOI_API void aoi_fire_message(struct aoi_space *space, char* message_id, void* userdata);
+AOI_API char* aoi_current_message_id(struct aoi_space *space);
 
 //neighbor
-int aoi_begin_parse_neighbor(struct aoi_space *space, uint32_t id);
-int aoi_next_neighbor(struct aoi_space *space, uint32_t* id);
-int aoi_end_parse_neighbor(struct aoi_space *space);
-int aoi_is_pair(struct aoi_space *space, uint32_t id, uint32_t tid);
+AOI_API int aoi_begin_parse_neighbor(struct aoi_space *space, uint32_t id);
+AOI_API int aoi_next_neighbor(struct aoi_space *space, uint32_t* id);
+AOI_API int aoi_end_parse_neighbor(struct aoi_space *space);
+AOI_API int aoi_is_pair(struct aoi_space *space, uint32_t id, uint32_t tid);
+
+#if defined(__cplusplus)
+}
+#endif
 
 #endif
